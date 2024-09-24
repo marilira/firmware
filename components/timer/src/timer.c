32d8299d5cca87b008312a73f27896fbc2866304
Parahybaja <parahybaja@gmail.com>
2024-09-24 18:03:43 -0300

    changed timer to decrement

--- components/timer/src/timer.c
+++ components/timer/src/timer.c
@@ -25,10 +25,11 @@ void task_timer(void *arg){
         float current_time = esp_timer_get_time()/1000000;
 
         int time = (int) (current_time - start_time);
+        int remaining_time = (4 * 60 * 60) - time;
 
-        hours.value = (float) (time / 3600);
-        minutes.value = (time / 60) - (hours.value * 60);
-        seconds.value = time - (minutes.value * 60);
+        hours.value = (float) (remaining_time / 3600);
+        minutes.value = (remaining_time / 60) - (hours.value * 60);
+        seconds.value = remaining_time - (minutes.value * 60) - (hours.value * 3600);
         
         xQueueSend(qh_hours, &hours, pdMS_TO_TICKS(0));
         xQueueSend(qh_minutes, &minutes, pdMS_TO_TICKS(0)); 
